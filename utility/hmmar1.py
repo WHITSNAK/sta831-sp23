07508177b95a83c283b1950baa096fbadf9dffd8
Ryan Tang <ztang484@gmail.com>
2023-02-12 12:29:47 -0500

    vectorized multi-chain sample step

--- utility/hmmar1.py
+++ utility/hmmar1.py
@@ -9,7 +9,7 @@ from typing import NamedTuple, List
 
 class HMMAR1:
     """HMM AR(1) model with Gaussian innovation and measurement error"""
-    class GaussianLatentState(NamedTuple):
+    class LatentState(NamedTuple):
         mean: float
         var: float
 
@@ -21,12 +21,12 @@ class HMMAR1:
         def s(self):
             return self.v / (1 - self.phi**2)
 
-    def __init__(self, init_state: GaussianLatentState):
+    def __init__(self, init_state: LatentState):
         self.x0 = init_state
         self.xt = init_state
-        self.states: List[HMMAR1.GaussianLatentState] = [init_state]
+        self.states: List[HMMAR1.LatentState] = [init_state]
 
-    def filter(self, new_y: float, theta: Parameter) -> GaussianLatentState:
+    def filter(self, new_y: float, theta: Parameter) -> LatentState:
         """Gaussian Forward Filter"""
         phi, v, w = theta.phi, theta.v, theta.w
 
@@ -37,30 +37,30 @@ class HMMAR1:
         new_mean = at + At * (new_y - at)
         new_var = w * At
 
-        new_state = HMMAR1.GaussianLatentState(mean=new_mean, var=new_var)
+        new_state = HMMAR1.LatentState(mean=new_mean, var=new_var)
         self.xt = new_state
         self.states.append(new_state)
         return new_state
 
-    def sample_trace(self, theta: Parameter) -> pd.Series:
+    def sample_trace(self, n_traces: int, theta: Parameter) -> np.array:
         """Sample the entire latent trace based on the current state"""
         size = len(self.states)
-        normal_samples = deque(normal(loc=0, scale=1, size=size))
+        normal_samples = deque(normal(loc=0, scale=1, size=(size, n_traces)))
 
         # process the last one first for a starting point
         x = self.states[-1].mean + np.sqrt(self.states[-1].var) * normal_samples.pop()
 
         trace = deque()
-        for i in range(len(self.states)-2, 0, -1): # working backward starting at the second last
-            state = self.states[i]
+        for i in range(len(self.states)-1, 0, -1): # working backward starting at the second last
+            state = self.states[i-1]
             state_post = self._get_backward_posterior(state, x, theta)
             standard_normal = normal_samples.pop()
             x = state_post.mean + np.sqrt(state_post.var) * standard_normal
             trace.appendleft(x)
 
-        return pd.Series(trace)
+        return np.array(trace)
     
-    def _get_backward_posterior(self, xt: GaussianLatentState, last_x, theta: Parameter) -> GaussianLatentState:
+    def _get_backward_posterior(self, xt: LatentState, last_x, theta: Parameter) -> LatentState:
         """Calculate the backward posterior p(x[t]|x[t+1], y[1:t]) based off the markov property"""
         phi, v = theta.phi, theta.v
 
@@ -70,4 +70,4 @@ class HMMAR1:
         new_mean = xt.mean + phi*At * (last_x - phi*xt.mean)
         new_var = v * At
 
-        return HMMAR1.GaussianLatentState(mean=new_mean, var=new_var)
+        return HMMAR1.LatentState(mean=new_mean, var=new_var)
