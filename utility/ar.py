de50c71189167ce69a12d814687aaf42bb4f2fdf
Ryan Tang <ztang484@gmail.com>
2023-02-19 21:28:37 -0500

    add logp for ar model

--- utility/ar.py
+++ utility/ar.py
@@ -15,20 +15,23 @@ class AR:
         self.lags = lags
     
     def fit(self, data) -> AR:
-        """Train the model and calculates all statistics"""
+        """
+        Train the model and calculates all statistics
+        It uses the conditional likelihood p(x[1:n]|θ, x[0])
+        """
         lags = self.lags
         data = data - data.mean()
-        n = data.shape[0]
 
         # construct design matrix
         y = data[lags:]
-        X = hankel(data[:n-lags], data[-(lags+1):-1])
+        X = self._get_design_matrix(data.values)
 
         # sufficient statistics
         B = X.T @ X
         b = np.linalg.inv(B) @ X.T @ y
 
         self.data_ = data
+        self.design_matrix_ = X
         self.B_ = B
         self.coeff_ = b
         self.resid_ = y - X@b
@@ -81,3 +84,46 @@ class AR:
             yhats.append(x)
             
         return np.array(yhats)
+
+    def _get_design_matrix(self, data: np.array) -> np.array:
+        """
+        Create the design matrix out of the time series based on the given lags
+
+        parameters:
+        -----------
+        data: the original time series with n observations row vector with shape (n, )
+
+        return:
+        -------
+        Deisgn Matrix X with shape (n-p, p)
+        """
+        lags = self.lags
+        n = data.shape[0]
+        X = hankel(data[:n-lags], data[-(lags+1):-1])
+        return X
+    
+    def logp(self, b_samples: np.array, v_samples: np.array) -> np.array:
+        """
+        The unnormalized log probability of all posterior samples
+
+        parameters:
+        -----------
+        b_samples: posterior samples of ϕ[p] with shape [m, p]. m for MC sample size.
+        v_samples: posterior samples of v with shape [m, 1]. m for MC sample size.
+
+        return:
+        -------
+        log probability array with shape (m, )
+        """
+        X = self.design_matrix_
+        x0 = self.data_[0]
+        n = X.shape[0]
+
+        logps = []
+        for phi, v in zip(b_samples, v_samples):
+            e = X - phi * X
+            Q = (1-phi**2)*x0**2 + e.T@e
+            logp = np.log(1-phi**2)/2 - (n/2)*np.log(v) - Q.flatten()[0]/(2*v)
+            logps.append(logp.flatten()[0])
+
+        return np.array(logps)
